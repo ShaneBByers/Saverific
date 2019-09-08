a6f70e7984e5558860b9cb2c0c151ca51328c6b8
shanebyers <_n4n75IhB_GtU1ng0ny4m4_>
2019-09-07 21:30:28 -0400

    Added logs to file manager.

--- FileDataManager.py
+++ FileDataManager.py
@@ -12,11 +12,17 @@ class FileDataManager:
 
     def open(self, file_name):
         self.file = open(file_name, "w+")
+        self.logger.info("Opened file " + file_name)
 
     def write(self, text="", tabs=0):
         for i in range(tabs):
             self.file.write(self.tab)
         self.file.write(text)
+        test = text.split("\n", 1)
+        if len(text.split("\n", 1)[0]) > 0:
+            self.logger.info("Wrote \"" + text.split("\n", 1)[0] + "\" to file.")
+        else:
+            self.logger.info("Wrote new line(s) to file.")
 
     def write_line(self, text="", tabs=0, lines=1):
         for i in range(lines):
@@ -25,6 +31,7 @@ class FileDataManager:
 
     def close(self):
         self.file.close()
+        self.logger.info("Closed file.")
 
     def __del__(self):
-        self.file.close()
+        self.close()
